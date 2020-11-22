06c785f91d75922060f47e63ab6bee50124fd3a3
alexandre <abour@localhost.com>
2020-11-22 18:12:35 +0100

    Implement fractal with parallelization

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # fractal
+
+## Overview
+
+![fractal](https://user-images.githubusercontent.com/1284598/99910694-b4f9ae80-2cef-11eb-8cdd-efea862d2158.jpg)
+
+100% inspired by this [golang implementation](https://github.com/kochampsy/fractal) and the associated [reddit post](https://www.reddit.com/r/golang/comments/jy9kq6/a_fractal_i_rendered_in_go_without_any_external/)
+
+## Build and run
+
+Please build in release mode for better performance
+
+```bash
+cargo build --release
+```
+
+Simply run it with the following command
+
+```bash
+./target/release/fractal
+```
+
+The generated image is named "fractal.png"
+
--- src/main.rs
+++ src/main.rs
@@ -2,73 +2,111 @@ extern crate image;
 pub mod color;
 
 use rand::{thread_rng, Rng};
+use std::sync::Arc;
+use tokio::net::TcpListener;
+use tokio::prelude::*;
+use tokio::sync::mpsc;
 
-fn main() {
-    let width: u32 = 512;
-    let height: u32 = 512;
-    let size = width * height * 3;
+#[tokio::main]
+async fn main() {
+    let blocking_task = tokio::spawn(async {
+        let width: u32 = 1024;
+        let height: u32 = 1024;
+        let buf_size = width * height * 3;
+        let nb_samples = 50;
+        let px = -0.5557506;
+        let py = -0.55560;
+        let size = 0.000000001;
+        let max_iter: u32 = 1000;
+        let mut buf: Vec<u8> = Vec::with_capacity(buf_size as usize);
+        buf.resize(buf_size as usize, 0);
 
-    let mut buf: Vec<u8> = Vec::with_capacity(size as usize);
-    buf.resize(size as usize, 0);
+        let (tx, mut rx) = mpsc::channel(100);
 
-    render(
-        &mut buf,
-        width,
-        height,
-        50,
-        -0.5557506,
-        -0.55560,
-        0.000000001,
-        1000,
-    );
+        for y in 0..height {
+            let tx = tx.clone();
+            tokio::spawn(async move {
+                let (mut line, line_number) =
+                    render_line(y, width, height, px, py, nb_samples, size, max_iter);
+                tx.send((line, line_number)).await.unwrap();
+            });
+        }
 
-    image::save_buffer("image.png", &buf, width, height, image::ColorType::Rgb8).unwrap();
+        drop(tx);
+
+        let mut finished: f64 = 0.;
+        while let Some(res) = rx.recv().await {
+            finished += 1.;
+            let percentage_finished = (finished / (height as f64)) * 100.;
+            println!("Progress: {}%", percentage_finished);
+
+            let (line, line_number) = res;
+            write_line(&mut buf, &line, line_number, width);
+        }
+        image::save_buffer("fractal.png", &buf, width, height, image::ColorType::Rgb8).unwrap();
+    });
+
+    blocking_task.await.unwrap();
 }
 
-fn render(
-    buf: &mut Vec<u8>,
-    width: u32,
-    height: u32,
-    nb_samples: i32,
-    px: f64,
-    py: f64,
-    size: f64,
-    max_iter: i32,
-) {
-    let mut rng = thread_rng();
-
-    for y in 0..height {
-        for x in 0..width {
-            let sampled_size = nb_samples * 3;
-            let mut sampled_colours: Vec<u8> = Vec::with_capacity(sampled_size as usize);
-            sampled_colours.resize(sampled_size as usize, 0);
-
-            for i in 0..nb_samples {
-                let nx = size * (((x as f64) + rng.gen_range(0., 1.0)) / (width as f64)) + px;
-                let ny = size * (((y as f64) + rng.gen_range(0., 1.0)) / (height as f64)) + py;
-                let (m_res, m_iter) = mandelbrot_iter(nx, ny, max_iter);
-                let (paint_r, paint_g, paint_b) = paint(m_res, m_iter);
-
-                sampled_colours[(i * 3) as usize] = paint_r;
-                sampled_colours[((i * 3) + 1) as usize] = paint_g;
-                sampled_colours[((i * 3) + 2) as usize] = paint_b;
-            }
-            let mut r: i32 = 0;
-            let mut g: i32 = 0;
-            let mut b: i32 = 0;
-            for i in 0..nb_samples {
-                r += (sampled_colours[(i * 3) as usize]) as i32;
-                g += (sampled_colours[((i * 3) + 1) as usize]) as i32;
-                b += (sampled_colours[((i * 3) + 2) as usize]) as i32;
-            }
-            buf[((y * width + x) * 3) as usize] = ((r as f64) / (nb_samples as f64)) as u8;
-            buf[(((y * width + x) * 3) + 1) as usize] = ((g as f64) / (nb_samples as f64)) as u8;
-            buf[(((y * width + x) * 3) + 2) as usize] = ((b as f64) / (nb_samples as f64)) as u8;
-        }
+fn write_line(buf: &mut Vec<u8>, line: &Vec<u8>, line_number: u32, width: u32) {
+    for i in 0..width {
+        buf[(((line_number * width) + i) * 3) as usize] = line[(i * 3) as usize];
+        buf[((((line_number * width) + i) * 3) + 1) as usize] = line[((i * 3) + 1) as usize];
+        buf[((((line_number * width) + i) * 3) + 2) as usize] = line[((i * 3) + 2) as usize];
     }
 }
 
-fn paint(r: f64, n: i32) -> (u8, u8, u8) {
+fn render_line(
+    line_number: u32,
+    width: u32,
+    height: u32,
+    px: f64,
+    py: f64,
+    nb_samples: u32,
+    size: f64,
+    max_iter: u32,
+) -> (Vec<u8>, u32) {
+    let mut rng = thread_rng();
+
+    let line_size = width * 3;
+    let mut line: Vec<u8> = Vec::with_capacity(line_size as usize);
+    line.resize(line_size as usize, 0);
+
+    for x in 0..width {
+        let sampled_size = nb_samples * 3;
+        let mut sampled_colours: Vec<u8> = Vec::with_capacity(sampled_size as usize);
+        sampled_colours.resize(sampled_size as usize, 0);
+
+        for i in 0..nb_samples {
+            let nx = size * (((x as f64) + rng.gen_range(0., 1.0)) / (width as f64)) + px;
+            let ny =
+                size * (((line_number as f64) + rng.gen_range(0., 1.0)) / (height as f64)) + py;
+            let (m_res, m_iter) = mandelbrot_iter(nx, ny, max_iter);
+            let (paint_r, paint_g, paint_b) = paint(m_res, m_iter);
+
+            sampled_colours[(i * 3) as usize] = paint_r;
+            sampled_colours[((i * 3) + 1) as usize] = paint_g;
+            sampled_colours[((i * 3) + 2) as usize] = paint_b;
+        }
+        let mut r: i32 = 0;
+        let mut g: i32 = 0;
+        let mut b: i32 = 0;
+        for i in 0..nb_samples {
+            r += (sampled_colours[(i * 3) as usize]) as i32;
+            g += (sampled_colours[((i * 3) + 1) as usize]) as i32;
+            b += (sampled_colours[((i * 3) + 2) as usize]) as i32;
+        }
+
+        line[(x * 3) as usize] = ((r as f64) / (nb_samples as f64)) as u8;
+        line[((x * 3) + 1) as usize] = ((g as f64) / (nb_samples as f64)) as u8;
+        line[((x * 3) + 2) as usize] = ((b as f64) / (nb_samples as f64)) as u8;
+    }
+
+    return (line, line_number);
+}
+
+fn paint(r: f64, n: u32) -> (u8, u8, u8) {
     if r > 4. {
         return color::hsl_to_rgb((n as f64) / 800. * r, 1., 0.5);
     } else {
@@ -76,7 +114,7 @@ fn paint(r: f64, n: i32) -> (u8, u8, u8) {
     }
 }
 
-fn mandelbrot_iter(px: f64, py: f64, max_iter: i32) -> (f64, i32) {
+fn mandelbrot_iter(px: f64, py: f64, max_iter: u32) -> (f64, u32) {
     let (mut x, mut y, mut xx, mut yy, mut xy) = (0., 0., 0., 0., 0.);
 
     for i in 0..max_iter {
